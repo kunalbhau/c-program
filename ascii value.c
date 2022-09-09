47c5be1a9bbf3ba19eac9a7542bd2a18d18b318e
kunal <84256660+kunalbhau@users.noreply.github.com>
2022-09-09 16:50:57 +0530

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,16 @@
+// program to find ascii value of alphabet//
+
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    
+    printf("enter an alphabet: ");
+    scanf("%c",&ch);
+
+    printf("ASCII value of % c is %d",ch, ch);
+
+    return 0;
+
+}
