959a640ed0e4d58d267aafca96fd1a7fbabe950f
kunal <84256660+kunalbhau@users.noreply.github.com>
2022-09-11 13:21:55 +0530

    Add files via upload

---
+++ teranry operator +-.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int num;
+    printf("enter a  number");
+    scanf("%d", &num);
+
+    {
+    num > 0?
+    printf("it is positive") : printf(" it is negative");
+    }
+    return 0;
+}
