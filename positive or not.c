d6ae6cdcf6bd0472565196af62e39fef0b78a96b
kunal <84256660+kunalbhau@users.noreply.github.com>
2022-09-11 13:08:35 +0530

    Add files via upload

---
+++ positive or not.c
@@ -0,0 +1,20 @@
+//c program to find whether a number is positive or not //
+
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",& num);
+
+
+    {
+        if 
+        (num > 0)
+        printf("number is positive");
+        else
+        printf("number is negative");
+    }
+    return 0;
+}
