0b3546d7b1defb4088f83cd210171b5d52dcbc87
kunal <84256660+kunalbhau@users.noreply.github.com>
2022-10-14 19:46:23 +0530

    Add files via upload

---
+++ count digits of integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ();
+{
+
+
+    int n ;
+    int count = 0;
+
+    printf("enter a number : ");
+    scanf("%d",& n);
+
+    while (n!= 0)
+
+    {
+        n=n/10 ;
+        count++;
+    }
+    printf("the number of digits in an integer is %d", count);
+
+    return 0;
+}
