212993358e80af2dba9200fe4a4d5b6163121fd9
kunal <84256660+kunalbhau@users.noreply.github.com>
2023-04-15 18:16:37 +0530

    Add files via upload

---
+++ program to print a string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+     char str[100];
+
+    printf("enter an string:");
+    scanf("%s", & str);
+
+    printf("the string is %s", str);
+
+    return 0;
+}
